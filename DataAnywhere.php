132c1e6ba0a0771c4b697f0eebd0956b8beeecf5
adam nunez <adam.a.nunez@gmail.com>
2022-11-21 13:19:47 -0600

    Code style standardization

--- DataAnywhere.php
+++ DataAnywhere.php
@@ -1,60 +1,66 @@
 <?php
 
 namespace UWMadison\DataAnywhere;
+
 use ExternalModules\AbstractExternalModule;
 use REDCap;
 
-class DataAnywhere extends AbstractExternalModule {
-    
-    private $module_global = 'DataAnywhere';
-    
-    public function redcap_every_page_top($project_id) {
+class DataAnywhere extends AbstractExternalModule
+{
+    public function redcap_every_page_top($project_id)
+    {
         // Custom Config page
-        if ( $this->isPage('ExternalModules/manager/project.php') && $project_id != NULL) {
-            $this->initGlobal();
+        if ($this->isPage('ExternalModules/manager/project.php') && $project_id != NULL) {
             $url = $this->getUrl('config.js');
             echo "<script src={$url}></script>";
         }
     }
-    
-    public function redcap_data_entry_form_top ( $project_id, $record, $instrument, $event_id) {
-        if ( empty($record) )
-            return; // New Record, no data
-        $this->initGlobal();
+
+    public function redcap_data_entry_form_top($project_id, $record, $instrument, $event_id)
+    {
+        if (empty($record)) return; // New Record, no data
         $settings = $this->getProjectSettings();
         $data = [];
-        
+
         // Loop over all of the config, destination-all is arbitrary 
-        foreach ( $settings['destination-all'] as $index => $destAll ) {
+        foreach ($settings['destination-all'] as $index => $destAll) {
             $destAll   = $destAll == "1";
             $destEvent = $settings['destination-event'][$index];
             $destInst  = $settings['destination-instrument'][$index];
-            
-            if ( $destAll || ( ($destEvent == $event_id) && ($destInst == $instrument) ) || 
-            ( empty($destEvent) && ($destInst == $instrument) ) || ( ($destEvent == $event_id) && empty($destInst) ) ) {
+
+            if (
+                $destAll || (($destEvent == $event_id) && ($destInst == $instrument)) ||
+                (empty($destEvent) && ($destInst == $instrument)) || (($destEvent == $event_id) && empty($destInst))
+            ) {
                 $srcAll   = $settings['source-all'][$index] == "1";
                 $srcEvent  = $settings['source-event'][$index];
                 $srcInst   = $settings['source-instrument'][$index];
                 $srcEvent = $srcAll ? null : $srcEvent;
                 $fields = $srcAll ? null : REDCap::getFieldNames($srcInst);
-                $tmp  = REDCap::getData($project_id,'array',$record,$fields,$srcEvent);
+                $tmp  = REDCap::getData($project_id, 'array', $record, $fields, $srcEvent);
                 $data = array_replace_recursive($data, $tmp);
             }
         }
-        
-        // Post data down to JS if we have any
-        if ( !empty($data) ) {
-            echo "<script>{$this->module_global}.data = ".json_encode($data[$record]).";</script>";
-            echo "<script>{$this->module_global}.eventNames = ".json_encode(REDCap::getEventNames(true)).";</script>";
+
+        // Pass data down to JS if we have any
+        if (!empty($data)) {
+            echo "<script> var DataAnywhere = " . json_encode([
+                "data" => $data[$record],
+                "eventNames" => REDCap::getEventNames(true),
+                "usernames" => $this->getUsers()
+            ]) . ";</script>";
         }
     }
-    
-    private function initGlobal() {
-        $data = json_encode([
-            "modulePrefix" => $this->getPrefix()
-        ]);
-        echo "<script>var {$this->module_global} = {$data};</script>";
+
+    private function getUsers()
+    {
+        $users = "\"" . implode("\",\"", REDCap::getUsers()) . "\"";
+        $sql = "SELECT username, CONCAT(user_firstname, ' ' , user_lastname) as name FROM redcap_user_information WHERE username in ($users)";
+        $data = db_query($sql);
+        $results = [];
+        while ($row = db_fetch_assoc($data)) {
+            $results[$row["username"]] = $row["name"];
+        }
+        return $results;
     }
 }
-
-?>
--- README.md
+++ README.md
@@ -2,20 +2,15 @@
 
 ## What does it do?
 
-DataAnywhere ships data from other events, specific instruments on other events, or all of a record's data to the current instrument as a javascript object for use by other External Modules such as Shazam. This allows for easy access to the data for use in custom JS.
+DataAnywhere ships data from other events, specific instruments on other events, or all of a record's data to the current instrument as a javascript object for use by other External Modules such as Shazam. This allows for easy access to the data for use in customizations.
 
-## Installing
-
-This EM isn't available to install via redcap's EM database so you'll need to install to your modules folder (i.e. `redcap/modules/data_anywhere_v1.0.0`) manually if you'd like to use it.
-
-## Configuration
-
-Configuration is simple: 
+Configuration & Usage is simple:
 
 1. Define data to share - This can be a specifc event or event/instrument combo or all data on the record.
 2. Define where that data is sent - Same specification as above
 3. Use data in the `DataAnywhere.data` object, data is organized by event_id and, if applicable, in the typical redcap 'repeat_instances' scheme.
+4. The `DataAnywhere.eventNames` and `DataAnywhere.usernames` object also exist to help with accessing the correct event_id or displaying a user's name in your customization
 
-## Call Outs
+## Installing
 
-* You cannot ship data from other records.
+You can install the module from the REDCap EM repo or drop it directly in your modules folder (i.e. `redcap/modules/data_anywhere_v1.0.0`).
